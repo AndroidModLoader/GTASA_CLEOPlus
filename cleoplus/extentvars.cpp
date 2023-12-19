4fc64feeded7ebbe6e2de1b1a52d373c437d4e72
RusJJ <mctesting1@yandex.ru>
2023-12-19 15:53:28 +0300

    NDK Differences moment

--- cleoplus/extentvars.cpp
+++ cleoplus/extentvars.cpp
@@ -4,51 +4,6 @@ CPool<PedExtVars> *ms_pPedExtVarsPool = NULL;
 CPool<VehicleExtVars> *ms_pVehicleExtVarsPool = NULL;
 CPool<ObjectExtVars> *ms_pObjectExtVarsPool = NULL;
 
-inline PedExtVars* GetExtData(CPed* ped)
-{
-    if(!ms_pPedExtVarsPool)
-    {
-        if(!*ms_pPedPool) return NULL;
-        auto size = (*ms_pPedPool)->m_nSize;
-        ms_pPedExtVarsPool = new CPool<PedExtVars>(size, "PedExtVars");
-        ms_pPedExtVarsPool->m_nFirstFree = size;
-        for(int i = 0; i < size; ++i) ms_pPedExtVarsPool->m_byteMap[i].bEmpty = false;
-    }
-    return ms_pPedExtVarsPool->GetAt((*ms_pPedPool)->GetIndex(ped));
-}
-inline VehicleExtVars* GetExtData(CVehicle* ped)
-{
-    if(!ms_pVehicleExtVarsPool)
-    {
-        if(!*ms_pVehiclePool) return NULL;
-        auto size = (*ms_pVehiclePool)->m_nSize;
-        ms_pVehicleExtVarsPool = new CPool<VehicleExtVars>(size, "VehicleExtVars");
-        ms_pVehicleExtVarsPool->m_nFirstFree = size;
-        for(int i = 0; i < size; ++i) ms_pVehicleExtVarsPool->m_byteMap[i].bEmpty = false;
-    }
-    return ms_pVehicleExtVarsPool->GetAt((*ms_pVehiclePool)->GetIndex(ped));
-}
-inline ObjectExtVars* GetExtData(CObject* ped)
-{
-    if(!ms_pObjectExtVarsPool)
-    {
-        if(!*ms_pObjectPool) return NULL;
-        auto size = (*ms_pObjectPool)->m_nSize;
-        ms_pObjectExtVarsPool = new CPool<ObjectExtVars>(size, "ObjectExtVars");
-        ms_pObjectExtVarsPool->m_nFirstFree = size;
-        for(int i = 0; i < size; ++i) ms_pObjectExtVarsPool->m_byteMap[i].bEmpty = false;
-    }
-    return ms_pObjectExtVarsPool->GetAt((*ms_pObjectPool)->GetIndex(ped));
-}
-inline ExtendedVars *FindExtendedVarsFromId(std::list<ExtendedVars*> extendedVarsList, uint32_t findId)
-{
-    for (ExtendedVars* extendedVars : extendedVarsList)
-    {
-        if (extendedVars->id == findId) return extendedVars;
-    }
-    return NULL;
-}
-
 CLEO_Fn(INIT_EXTENDED_CHAR_VARS)
 {
     char buf[100];
